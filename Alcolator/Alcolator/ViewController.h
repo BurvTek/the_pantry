6fc53c3a70f2aa3cd8fc5cd3255319419c09fa9f
Paul Burville <paul@imls.com>
2016-06-15 00:49:09 -0400

    Checkpoint 19 Alcolator real submission

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Paul Burville on 6/15/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
