010d4ff2a8457a8b6c772e2fc811af20a3ee22a7
Paul Burville <paul@imls.com>
2016-07-12 22:56:02 -0400

    Checkpoint 21 Alcolator: completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Paul Burville on 7/12/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
